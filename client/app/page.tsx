11230b9dffbf0c6efa824d4ea703aa675410cb2b
GamerBachu <rajnisingh.mrs@gmail.com>
2025-04-19 13:35:44 +0530

    Initial setup

---
+++ client/app/page.tsx
@@ -0,0 +1,13 @@
+import DeleteIcon from "@mui/icons-material/Delete";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import { Container } from "@mui/material";
+
+export default function Home() {
+  return (
+    <Container maxWidth="lg">
+      <h1>Home</h1>
+        <DeleteIcon />
+        <DeleteForeverIcon />
+    </Container>
+  );
+}
---
+++ client/app/template.tsx
@@ -0,0 +1,13 @@
+import DeleteIcon from "@mui/icons-material/Delete";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import { Container } from "@mui/material";
+
+export default function Template() {
+  return (
+    <Container maxWidth="lg">
+      <h1>Template</h1>
+        <DeleteIcon />
+        <DeleteForeverIcon />
+    </Container>
+  );
+}
