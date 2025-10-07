5f4b55286c102edd5d848ec9154d09e7063595dc
GamerBachu <rajnisingh.mrs@gmail.com>
2025-10-07 20:26:14 +0530

    fix home page

--- client/src/pages/dashboard/Dashboard.tsx
+++ client/src/pages/dashboard/Dashboard.tsx
@@ -4,9 +4,29 @@ import { useAppSession } from "../../contexts";
 const Dashboard = () => {
   const appSession = useAppSession();
 
- 
+  console.log(appSession);
 
-  return <div>Dashboard{ appSession.info.isAuthorized}</div>;
+  return (
+    <div className="card m-1">
+      <div className="card-header d-flex">
+        <div className="h2 me-auto"  >
+          {appSession.info.account?.name}
+        </div>
+
+      </div>
+      <div className="card-body">
+        <p>
+          {appSession.info.account?.name}
+        </p>
+        <p>
+          {appSession.info.account?.email}
+        </p>
+        <p>
+          {appSession.info.account?.username}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
