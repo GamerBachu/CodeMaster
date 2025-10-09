f0198b8e9aa714f3f7cb16e5dd19c57bee42756a
GamerBachu <rajnisingh.mrs@gmail.com>
2025-10-09 21:52:38 +0530

    top bar css

--- client/src/components/HeaderContainer.tsx
+++ client/src/components/HeaderContainer.tsx
@@ -3,60 +3,88 @@ import appRoute from "../routes/appRoute";
 import { useAppSession } from "../contexts";
 import { useEffect, useState } from "react";
 const HeaderContainer = () => {
-
   const appSession = useAppSession();
 
-  const [authLink, setAuthLink] = useState([
-    { id: 10, path: appRoute.DASHBOARD.path, value: appRoute.DASHBOARD.value, active: true },
-    { id: 20, path: (appRoute.PLAN_Action.path + "/list"), value: appRoute.PLAN_Action.value, active: false },
-    { id: 30, path: (appRoute.POS_Action.path + "/list"), value: appRoute.POS_Action.value, active: false },
-    { id: 80, path: appRoute.PROFILE.path, value: appRoute.PROFILE.value, active: false },
-    { id: 90, path: appRoute.DASHBOARD.path, value: appRoute.DASHBOARD.value, active: false },
-    { id: 290, path: appRoute.SAMPLE_CANVAS.path, value: appRoute.SAMPLE_CANVAS.value, active: false },
-  ]);
+  const isValidUser = appSession?.info?.isAuthorized;
 
+  const [authLink, setAuthLink] = useState([
+    {
+      id: 10,
+      path: appRoute.DASHBOARD.path,
+      value: appRoute.DASHBOARD.value,
+      active: true,
+    },
+    {
+      id: 20,
+      path: appRoute.PLAN_Action.path + "/list",
+      value: appRoute.PLAN_Action.value,
+      active: false,
+    },
+    {
+      id: 30,
+      path: appRoute.POS_Action.path + "/list",
+      value: appRoute.POS_Action.value,
+      active: false,
+    },
+    {
+      id: 80,
+      path: appRoute.PROFILE.path,
+      value: appRoute.PROFILE.value,
+      active: false,
+    },
+    {
+      id: 90,
+      path: appRoute.DASHBOARD.path,
+      value: appRoute.DASHBOARD.value,
+      active: false,
+    },
+    {
+      id: 290,
+      path: appRoute.SAMPLE_CANVAS.path,
+      value: appRoute.SAMPLE_CANVAS.value,
+      active: false,
+    },
+  ]);
 
   const linkClick = (id: number) => {
     const d = authLink;
-    setAuthLink(d.map((link) => { link.active = link.id === id; return link; }));
+    setAuthLink(
+      d.map((link) => {
+        link.active = link.id === id;
+        return link;
+      })
+    );
   };
 
-
   useEffect(() => {
-
+    if (isValidUser === false) return;
     const getUrl = window.location.pathname;
-    setAuthLink(prev => prev.map((a) => {
+    setAuthLink((prev) =>
+      prev.map((a) => {
+        if (a.path.startsWith(getUrl)) a.active = true;
+        else a.active = false;
+        return a;
+      })
+    );
+  }, [isValidUser]);
 
-      if (a.path.startsWith(getUrl)) a.active = true;
-      else a.active = false;
-      return a;
-    }));
-
-
-  }, []);
-
-
-
-
-
-
-  if (appSession.info.isAuthorized)
+  if (isValidUser)
     return (
       <nav className="wrapper-header bg-body-tertiary border-bottom">
         <div className="d-flex flex-wrap">
           <ul className="nav me-auto">
-            {
-              authLink.map((link) => <li className="nav-item" key={link.id}>
+            {authLink.map((link) => (
+              <li className="nav-item" key={link.id}>
                 <Link
-
                   to={link.path}
-                  className={`nav-link link-body-emphasis px-2 text-capitalize ${link.active ? "active" : ""}`}
+                  className={`nav-link link-body-emphasis px-2 text-capitalize ${link.active ? "active" : ""
+                    }`}
                   onClick={() => linkClick(link.id)}
                 >
                   {link.value}
                 </Link>
-              </li>)
-            }
+              </li>
+            ))}
           </ul>
 
           <ul className="nav">
